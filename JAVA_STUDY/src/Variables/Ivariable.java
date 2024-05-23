1230ef80b89c1a1c5e0ae3096971c8cc5c648f63
arjunk3110 <arjunk3110@gmail.com>
2024-05-23 17:10:14 +0530

    First Commit

---
+++ JAVA_STUDY/src/Variables/Ivariable.java
@@ -0,0 +1,25 @@
+package Variables;
+
+public class Ivariable
+{
+	
+	int InstanceVariable=10;
+	
+
+	public static void main(String[] args)
+	{
+		
+		Ivariable obj= new Ivariable();
+		System.out.println(obj.InstanceVariable);
+		
+	}
+
+}
+
+
+
+
+
+
+
+
